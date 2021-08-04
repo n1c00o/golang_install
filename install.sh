585ca8a985d38ceabcb1dac5d6bdc67e7d33f444
n1c00o <git.n1c00o@gmail.com>
2021-08-04 21:54:36 +0200

    Fix code logic problems

--- install.sh
+++ install.sh
@@ -3,70 +3,63 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-get_os() {
-  case $(uname -s) in
-  MSYS_NT*)
-    return "windows"
-    ;;
-  Linux)
-    return "linux"
-    ;;
-  Darwin)
-    return "darwin"
-    ;;
-  *)
-    echo "Error: OS $(uname -s) is not supported by the script." 1>&2
-    exit 1
-    ;;
-  esac
-}
-
-get_arch() {
-  case $(uname -m) in
-  i*)
-    return "386"
-    ;;
-  x*)
-    return "amd64"
-    ;;
-  aarch64)
-    return "arm64"
-    ;;
-  *)
-    echo "Error: Arch $(uname -m) is not supported by the script." 1>&2
-    exit 1
-    ;;
-  esac
-}
-
-get_old_installation_version() {
-  if $(which go &>/dev/null); then
-    return go version | grep -oP "go[0-9.]+"
-  else
-    return ""
-  fi
-}
-
 GOPATH="$HOME/go"
 GOUTIL="$HOME/.go"
-LATEST="$(curl https://golang.org/VERSION?m=text)"
-OS=get_os
-if [ os in "windows"]; then
+LATEST="$(curl -sS https://golang.org/VERSION?m=text)"
+
+case $(uname -s) in
+MSYS_NT*)
+  OS="windows"
+  ;;
+Linux)
+  OS="linux"
+  ;;
+Darwin)
+  OS="darwin"
+  ;;
+*)
+  echo "Error: OS $(uname -s) is not supported by the script." 1>&2
+  exit 1
+  ;;
+esac
+
+if [[ os == "windows" ]]; then
   PKG_END=zip
 else
   PKG_END=tar.gz
 fi
-ARCH=get_arch
+
+case $(uname -m) in
+i*)
+  ARCH="386"
+  ;;
+x*)
+  ARCH="amd64"
+  ;;
+aarch64)
+  ARCH="arm64"
+  ;;
+*)
+  echo "Error: Arch $(uname -m) is not supported by the script." 1>&2
+  exit 1
+  ;;
+esac
+
 PKG="$LATEST.$OS-$ARCH.$PKG_END"
 URL="https://golang.org/dl/$PKG"
 
 mkdir -p "$GOUTIL"
 
+if $(which go &>/dev/null); then
+  OLD_GO_VERSION=$(go version | grep -oP "go[0-9.]+")
+else
+  OLD_GO_VERSION=""
+fi
+
 # Check for cache
 # if not, download and cache
 # otherwise, simply copy
-
-OLD_GO_VERSION="$(get_old_installation_version)"
+echo "Checking for existing installation in $GOPATH"
 
 if ! [[ "$OLD_GO_VERSION" == "$LATEST" ]]; then
   if [ -z "$OLD_GO_VERSION" ]; then
@@ -77,9 +70,9 @@ if ! [[ "$OLD_GO_VERSION" == "$LATEST" ]]; then
     rm -rf "$GOPATH"
   fi
 
-  if ! [ -z "$GOUTIL/$PKG"]; then
+  if ! [ -e "$GOUTIL/$PKG" ]; then
     echo "Downloading version $LATEST for $ARCH on $OS..."
-    wget -t=5 -w=3 --continue --show-progress --progress=dot "$DL_URL" -P "$GOUTIL"
+    wget -t=5 --random-wait --continue --show-progress --progress=bar:force "$URL" -P "$GOUTIL"
   fi
 
   echo "Unpacking latest version $LATEST into $GOPATH..."
@@ -88,7 +81,7 @@ if ! [[ "$OLD_GO_VERSION" == "$LATEST" ]]; then
   echo "Installed Go $LATEST successfully!"
   echo "Manually add the following to your shell profile (.bashrc, .zshrc...)"
   echo "  export GOPATH=\"$GOPATH\""
-  echo "  export PATH=\"$GOPATH/bin:/usr/local/go/bin:$PATH\""
+  echo "  export PATH=\"$GOPATH/bin:/usr/local/go/bin:\$PATH\""
 
 else
   echo "Current Go installation is up to date ($LATEST for $ARCH on $OS)!"
